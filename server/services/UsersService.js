37d65e2ffbd7fbad4f3e0c4b7812e1382a57ef36
Scott T Lyman <s.t.lyman84@gmail.com>
2022-03-04 19:37:48 -0700

    commit

--- server/services/UsersService.js
+++ server/services/UsersService.js
@@ -1,8 +1,24 @@
 import { dbContext } from "../db/DbContext"
+import { BadRequest } from "../utils/Errors"
 
 class UsersService {
   async getAll(query) {
-    const users = await dbContext
+    const users = await dbContext.Users.find(query)
+    return users
+  }
+
+  async getById(id) {
+    const user = await dbContext.Users.findById(id)
+    if (!user) {
+      throw new BadRequest('Invalid User')
+    }
+    return user
+  }
+
+  async create(newUser) {
+    const user = await dbContext.Users.create(newUser)
+    await user.populate('creator')
+    return user
   }
 
 }
