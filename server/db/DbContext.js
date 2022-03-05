3ad82b27baef559507705306d53421636ba64c38
TamraPeterson <peterson.tamra@gmail.com>
2022-03-04 20:01:19 -0700

    delete dbcontext users

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -3,14 +3,12 @@ import { AccountSchema, ProfileSchema } from '../models/Account'
 import { CommentSchema } from "../models/Comment";
 import { LikeSchema } from "../models/Like";
 import { PhotoSchema } from "../models/Photo";
-import { UserSchema } from "../models/User";
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Photos = mongoose.model('Photo', PhotoSchema)
   Comments = mongoose.model('Comment', CommentSchema)
   Likes = mongoose.model('Like', LikeSchema)
-  Users = mongoose.model('User', UserSchema)
 
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
